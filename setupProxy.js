e55f5e59bdbed812f59406e3fd10c1414c73c8bc
Dayne <dayne.voller@outlook.com>
2021-10-14 15:07:17 +0200

    updated .env file

--- setupProxy.js
+++ setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/login", "/signup", "/basic", "/med"], { target: "https://micardstaging.herokuapp.com" })
+    createProxyMiddleware(["/login", "/signup", "/basic", "/med"], { target: "https://radiant-cliffs-18017.herokuapp.com/" })
   );
 };
